dbea38a4873d2ad87f2e3d1082b2ab38d9943797
12singhprakhar <12singhprakhar@gmail.com>
2025-05-21 10:55:03 +0530

    . importpages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import {Routes , Route} from 'react-router';
 import Homepage from './pages/Homepage'
+import Navbar from './components/Navbar'
+import Loginpage from'./pages/Loginpage'
+import Register from './pages/Register'
+import Messaging from './pages/Messaging'
 function App() {
  
 
